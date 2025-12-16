688da21097e20fa6d54697ca9753fb0eaf384bb9
blinx <111649405+tracyxmr@users.noreply.github.com>
2025-12-16 00:46:41 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-# Create the build directory.
-mkdir "bin"
-# Cd into build, if this fails exit
-cd "bin" || exit
-# Configure cmake
-cmake ..
-# Build Juno for release
-cmake --build . --config Release
